49543346f64c65ffa63ed68cb89300c1f5764924
Masaki Ishiko <4orestelements@gmail.com>
2020-01-07 16:31:32 +0900

    add comment

--- players/gameInfo.hh
+++ players/gameInfo.hh
@@ -1,6 +1,6 @@
-#include <set>
-#include <map>
 #include <iostream>
+#include <map>
+#include <set>
 
 using namespace std;
 
@@ -16,19 +16,20 @@ struct Cell {
 ostream &operator<<(ostream &out, const Cell &cell);
 
 struct GameInfo {
-  int id;
-  int size;
-  int step;
-  int maxSteps;
-  set <Cell> holes;
-  map <Cell, int> revealedTreasure;
-  map <Cell, int> sensedTreasure;
-  Cell positions[4];
-  int plans[4];
-  int actions[4];
-  int scores[2];
-  int remaining;
-  int thinkTime;
+  int id;                           // エージェントの番号
+  int size;                         // 競技場のサイズ
+  int step;                         // ステップ番号(0始まり)
+  int maxSteps;                     // 最大ステップ番号
+  set<Cell> holes;                  // 穴のあるセルの位置のリスト
+  map<Cell, int> revealedTreasure;  // 公知の埋蔵金
+  map<Cell, int> sensedTreasure;    // 感知した埋蔵金
+  Cell positions[4];  // エージェントの位置 (0: 侍(Aチーム), 1: 侍(Bチーム), 2:
+                      // 犬(Aチーム), 3: 犬(Bチーム))
+  int plans[4];    // 直前ステップでのプレイプラン
+  int actions[4];  // 直前ステップでの行動
+  int scores[2];   // スコア
+  int remaining;   // 残る埋蔵金
+  int thinkTime;   // 残る考慮時間
   GameInfo(istream &in);
 };
 
