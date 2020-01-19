59ae2b8cecadd3232fc3f1690c7eb7164c10d82a
Masaki Ishiko <4orestelements@gmail.com>
2020-01-19 18:13:33 +0900

    format

--- players/greedySamurai.cc
+++ players/greedySamurai.cc
@@ -32,26 +32,24 @@ int planSamurai(GameInfo &info) {
     int closest = numeric_limits<int>::max();
     for (auto n: myCell.fourNeighbors) {
       for (auto g: info.revealedTreasure) {
-	if (noAgentsIn(n->position, info)) {
-	  int dist =
-	    samuraiDistance(n, &cells[g.first.x][g.first.y], info.holes);
-	  if (dist <= closest) {
-	    if (dist != closest) {
-	      candidates.clear();
-	      closest = dist;
-	    }
-	    candidates.push_back(pair<Cell, int>(n->position, g.second));
-	  }
-	}
+	      if (noAgentsIn(n->position, info)) {
+	        int dist = samuraiDistance(n, &cells[g.first.x][g.first.y], info.holes);
+	        if (dist <= closest) {
+	          if (dist != closest) {
+	            candidates.clear();
+	            closest = dist;
+	          }
+	          candidates.push_back(pair<Cell, int>(n->position, g.second));
+	        }
+	      }
       }
     }
     if (!candidates.empty()) {
-      sort(candidates.begin(), candidates.end(),
-	   [](auto c1, auto c2) { return c1.second > c2.second; });
+      sort(candidates.begin(), candidates.end(), [](auto c1, auto c2) { return c1.second > c2.second; });
       for (auto c: candidates) {
-	int plan = directionOf(pos, c.first) +
-	  (info.holes.count(c.first) == 0 ? 0 : 16);
-	if (plan != avoidPlan) return plan;
+	      int plan = directionOf(pos, c.first) +
+	                 (info.holes.count(c.first) == 0 ? 0 : 16);
+	      if (plan != avoidPlan) return plan;
       }
     }
   }
@@ -65,12 +63,12 @@ int planSamurai(GameInfo &info) {
     if (noAgentsIn(n->position, info)) {
       int dist = samuraiDistance(n, &dogPosInfo, info.holes);
       if (dist < closest) {
-	int plan  = directionOf(pos, n->position) +
-	  (info.holes.count(n->position) == 0 ? 0 : 16);;
-	if (plan != avoidPlan) {
-	  bestPlan = plan;
-	  closest = dist;
-	}
+	      int plan  = directionOf(pos, n->position) +
+	                  (info.holes.count(n->position) == 0 ? 0 : 16);;
+	      if (plan != avoidPlan) {
+	        bestPlan = plan;
+	        closest = dist;
+	      }
       }
     }
   }
